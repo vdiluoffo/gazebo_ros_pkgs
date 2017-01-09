1559c00c2a8eada24d051ff285a0ec0d8653343c
vdiluoffo <vjd215@gmail.com>
2017-01-09 16:26:44 -0500

    Reference issue #512

--- gazebo_plugins/src/gazebo_plugins/gazebo_plugins_interface.py
+++ gazebo_plugins/src/gazebo_plugins/gazebo_plugins_interface.py
@@ -1,11 +1,20 @@
 #! /usr/bin/env python
 # Wrappers around the services provided by gazebo_ros_factory plugin
 
-import roslib; roslib.load_manifest('gazebo_plugins')
+
+####            ROS1 Code                       ####
+#import roslib; roslib.load_manifest('gazebo_plugins')
+
+
+#import rospy
 
 import sys
+from time import sleep
 
-import rospy
+import rclpy
+
+from std_msgs.msg import String
+
 from gazebo_plugins.msg import GazeboModel
 from gazebo_plugins.srv import SpawnModel
 from gazebo_plugins.srv import DeleteModel
@@ -13,12 +22,16 @@ from geometry_msgs.msg import Pose, Point, Quaternion
 
 def load_model(model_msg):
     print "waiting for service spawn_model"
-    rospy.wait_for_service('spawn_model')
+#    rospy.wait_for_service('spawn_model') Not available in rclpy.c ( // TODO(jacquelinekay) Services.)
+#    rclpy.wait_for_service('spawn_model')
     try:
-        spawn_model= rospy.ServiceProxy('spawn_model', SpawnModel)
+#        spawn_model= rospy.ServiceProxy('spawn_model', SpawnModel)
+
+#        spawn_model= rclpy.ServiceProxy('spawn_model', SpawnModel) Mot available in rclpy.c rclpy.ServiceProxy
         resp1 = spawn_model(model_msg)
         return resp1.success
-    except rospy.ServiceException, e:
+#    except rospy.ServiceException, e:
+    except rclpy.ServiceException, e:
         print "Service call failed: %s"%e
 
 
